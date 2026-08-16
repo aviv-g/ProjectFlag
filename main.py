272d5bda8dc5d391c80b7b0e8b99252b447a1f34
gili <kotlergili199@gmail.com>
2026-08-16 12:10:09 +0300

    print

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Hi")
+import consts
+import pygame
+import screen
+
+
+screen = pygame.display.set_mode((consts.WINDOW_HEIGHT, consts.WINDOW_WIDTH))
+screen.fill(consts.BLACK)
+
+
+
+# user actions
+def user_events_mouse():
+    global status
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            status = False
+
+status = True
+
+
+def main():
+    pygame.init()
+
+    image = pygame.image.load("soldier.png").convert()
+    soldier = pygame.transform.scale(image, (100, 40))
+    screen.blit(soldier, (100, 100))
+    pygame.display.update()
+
+
+    while status:
+        user_events_mouse()
+
+main()
+
