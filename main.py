30e92b25e436411ed804365b2baa4cc4b1f7c01c
jbt <grossmanshemeraviv@gmail.com>
2026-08-16 12:26:37 +0300

    draw soldier

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import pygame
 import screen
 
 
-screen = pygame.display.set_mode((consts.WINDOW_HEIGHT, consts.WINDOW_WIDTH))
-screen.fill(consts.BLACK)
-
-
 
 def user_events_mouse():
     global status
@@ -31,13 +27,9 @@ status = True
 def main():
     pygame.init()
 
-    image = pygame.image.load("soldier.png").convert()
-    soldier = pygame.transform.scale(image, (100, 40))
-    screen.blit(soldier, (100, 100))
-    pygame.display.update()
-
 
     while status:
         user_events_mouse()
+        screen.soldier()
 
 main()
--- screen.py
+++ screen.py
@@ -4,6 +4,12 @@ import pygame
 screen = pygame.display.set_mode((consts.WINDOW_HEIGHT, consts.WINDOW_WIDTH))
 screen.fill(consts.BLACK)
 
+def soldier():
+    image = pygame.image.load("soldier.png").convert()
+    soldier = pygame.transform.scale(image, (100, 40))
+    screen.blit(soldier, (100, 100))
+    pygame.display.update()
+
 
 #pygame.display.flip()
 #pygame.time.delay(1000)
