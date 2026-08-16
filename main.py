43124a983098bab2d87d95b0b8fc5ec7fc2b70d9
jbt <grossmanshemeraviv@gmail.com>
2026-08-16 11:41:02 +0300

    base screen

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hi")
+import consts
+import pygame
+
+
+screen = pygame.display.set_mode((consts.WINDOW_HEIGHT, consts.WINDOW_WIDTH))
+screen.fill(consts.BLACK)
+
+
+# user actions
+def user_events_mouse():
+    global status
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            status = False
+
+status = True
+
+
+def main():
+    pygame.init()
+
+    while status:
+        user_events_mouse()
+
+main()
---
+++ screen.py
@@ -0,0 +1,9 @@
+import consts
+import pygame
+
+screen = pygame.display.set_mode((consts.WINDOW_HEIGHT, consts.WINDOW_WIDTH))
+screen.fill(consts.BLACK)
+
+
+#pygame.display.flip()
+#pygame.time.delay(1000)
