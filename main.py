32dd2b5f1ef3847ab87d9f6ba3d5d48ca743d21e
jbt <grossmanshemeraviv@gmail.com>
2026-08-16 11:51:18 +0300

    user events keypad

--- main.py
+++ main.py
@@ -12,6 +12,19 @@ def user_events_mouse():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             status = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("left")
+            if event.key == pygame.K_RIGHT:
+                print("right")
+            if event.key == pygame.K_UP:
+                print("up")
+            if event.key == pygame.K_DOWN:
+                print("down")
+            if event.key == pygame.K_RETURN:
+                print("enter")
+
+
 
 status = True
 
