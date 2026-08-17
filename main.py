f61a6f03c8102df8760b81fe0741f2d6d3ce1190
jbt <grossmanshemeraviv@gmail.com>
2026-08-17 14:52:13 +0300

    k

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import game_field
 status = True
 keypad = {"pressed": False, "key" : "none"}
 
-game_status = consts.GAME
-
 def user_events_mouse():
     global status
     global keypad
@@ -40,29 +38,37 @@ def main():
     bush_list = game_field.search_bushes(game_field.bush_field)
 
     while status:
-        user_events_mouse()
-        screen.create_green_field(bush_list)
-        #screen.start_message()
+        game_status = consts.GAME
+        while game_status == consts.GAME:
+            user_events_mouse()
+            screen.create_green_field(bush_list)
+            #screen.start_message()
 
-        if keypad["pressed"]:
-            if keypad["key"] == "left":
-                print(soldier.left())
+            if keypad["pressed"]:
+                if keypad["key"] == "left":
+                    game_status, location = soldier.left()
 
-            if keypad["key"] == "right":
-                print(soldier.right())
+                if keypad["key"] == "right":
+                    game_status, location = soldier.right()
 
-            if keypad["key"] == "up":
-                print(soldier.up())
+                if keypad["key"] == "up":
+                    game_status, location = soldier.up()
 
-            if keypad["key"] == "down":
-                print(soldier.down())
+                if keypad["key"] == "down":
+                    game_status, location = soldier.down()
 
-            if keypad["key"] == "enter":
-                screen.create_field(landmine_list)
-                pygame.time.delay(1000)
+                if keypad["key"] == "enter":
+                    screen.create_field(landmine_list)
+                    pygame.time.delay(1000)
 
-            keypad["pressed"] = False
-            keypad["key"] = "none"
+                keypad["pressed"] = False
+                keypad["key"] = "none"
+
+
+        if game_status == consts.WIN:
+            print("Win")
+        else:
+            print("Lose")
 
         #screen.create_green_field(bush_list)
 
