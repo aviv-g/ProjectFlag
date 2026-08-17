c21d284bb14d3200abe595d4cba84336076c4756
gili <kotlergili199@gmail.com>
2026-08-17 10:55:06 +0300

    gfhfgh

--- main.py
+++ main.py
@@ -10,18 +10,28 @@ def user_events_mouse():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             status = False
+
+
+def user_events_keyboard():
+    global status
+    for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
+            print("here")
             if event.key == pygame.K_LEFT:
-                soldier.move_left()
-                #print("left")
-            if event.key == pygame.K_RIGHT:
-                soldier.move_right()
-            if event.key == pygame.K_UP:
-                soldier.move_up()
-            if event.key == pygame.K_DOWN:
-                soldier.move_down()
-            if event.key == pygame.K_RETURN:
+                x, y = soldier.move_left()
+                print(x, y)
+            elif event.key == pygame.K_RIGHT:
+                x, y = soldier.move_right()
+                print(x, y)
+            elif event.key == pygame.K_UP:
+                x, y = soldier.move_up()
+                print(x, y)
+            elif event.key == pygame.K_DOWN:
+                x, y = soldier.move_down()
+                print(x, y)
+            elif event.key == pygame.K_RETURN:
                 print("enter")
+
 
 status = True
 
@@ -34,7 +44,12 @@ def main():
 
     while status:
         user_events_mouse()
+        user_events_keyboard()
+
+
+
         screen.create_field(landmine_list)
+        #soldier.move_soldier()
 
 
 main()
